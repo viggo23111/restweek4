635bb8f3838b47125c86909a903352d198f1f326
viggo23111 <78900579+viggo23111@users.noreply.github.com>
2022-03-03 13:30:01 +0100

    works with fetch

--- src/main/java/rest/ApplicationConfig.java
+++ src/main/java/rest/ApplicationConfig.java
@@ -24,6 +24,7 @@ public class ApplicationConfig extends Application {
         resources.add(errorhandling.PersonNotFoundMapper.class);
         resources.add(org.glassfish.jersey.server.wadl.internal.WadlResource.class);
         resources.add(rest.PersonResource.class);
+        resources.add(rest.CorsFilter.class);
     }
     
 }
